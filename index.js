1879dd75edfad478cea09e71278ff2f8cce7c797
Shane <shane.black@hey.com>
2021-07-16 14:45:43 +1000

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+const paragraph = document.getElementById('text');
+document.addEventListener("DOMContentLoaded", function()    {
+     paragraph.innerHTML = "<p>This is really cool!</p>"
+})
